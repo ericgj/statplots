20faa1e6d114a33a48b2ce4854770a7e7961dbe0
Eric Gjertsen <ericgj72@gmail.com>
2015-12-28 15:18:21 -0500

    initial commit

---
+++ plot/ascii.js
@@ -0,0 +1,231 @@
+'use strict';
+
+var quantile = require('simple-statistics/src/quantile');
+var mean = require('simple-statistics/src/mean');
+var numericSort = require('simple-statistics/src/numeric_sort');
+
+
+var pluck = require('ramda/src/pluck');
+var join = require('ramda/src/join');
+var merge = require('ramda/src/merge');
+var reduce = require('ramda/src/reduce');
+var flip = require('ramda/src/flip');
+var call = require('ramda/src/call');
+var head = require('ramda/src/head');
+var last = require('ramda/src/last');
+
+var QUANTILES = [0,0.02,0.09,0.25,0.5,0.75,0.91,0.98,1];
+var MAXWIDTH = 80;
+var MAXHEIGHT = 40;
+var DEFAULTS = {
+  whiskers: tukeyWhiskers
+}
+
+module.exports = function(options,x,y,data){
+  var opts = merge(DEFAULTS,options);
+ 
+  /* temporary */
+  var vector = pluck('y',data);
+  var canvas = zeroArray(' ',MAXWIDTH * 5);
+
+  var plot = single(opts,'TESTING',canvas,vector);
+  return segment(MAXWIDTH,plot).map(join(''));
+}
+
+function single(options,title,canvas,vector){
+
+  var qs = quantile(vector, QUANTILES);
+  var d_range = range(qs);
+  var d_iqr = iqr(qs);
+  var d_median = median(qs);
+  var d_whiskers = options.whiskers(qs,vector);
+  var d_outliers = outliers(d_whiskers,vector);
+  var d_mean = mean(vector);
+  
+  /*
+  console.log('qs %o', qs);
+  console.log('range %o', d_range);
+  console.log('iqr %o', d_iqr);
+  console.log('whiskers %o', d_whiskers);
+  console.log('outliers %o', d_outliers);
+  */
+
+  var yscale = scale(d_range, [0, MAXWIDTH-1]);
+
+  var plotters = [ 
+      plotWhiskers( d_whiskers.map(yscale), d_iqr.map(yscale) ),
+      plotIQR( d_iqr.map(yscale) ),
+      plotMedian( yscale(d_median) ),
+      plotMean( yscale(d_mean) ),
+      plotOutliers( numericSort(d_outliers.map(yscale)) )
+  ]
+
+  return reduce( flip(call), canvas , plotters);
+}
+
+
+function plotWhiskers(w,r){
+  return function(canvas){
+    var lines = Math.floor( canvas.length / MAXWIDTH );
+    var midline = Math.floor( lines / 2 );
+
+    canvas[ (midline*MAXWIDTH) + w[0] ] = '|';
+    canvas[ (midline*MAXWIDTH) + w[1] ] = '|';
+    
+    for (var j=w[0]+1; j<r[0]; ++j){
+      canvas[ (midline*MAXWIDTH) + j ] = '-';
+    }
+    for (var j=r[1]+1; j<w[1]; ++j){
+      canvas[ (midline*MAXWIDTH) + j ] = '-';
+    }
+
+    return canvas;
+  }
+}
+
+function plotIQR(r){
+  return function(canvas){
+    var lines = Math.floor( canvas.length / MAXWIDTH );
+   
+    for (var i=0;i<lines;++i){
+      canvas[ (i*MAXWIDTH) + r[0] ] = '|';
+      canvas[ (i*MAXWIDTH) + r[1] ] = '|';
+      for (var j=r[0]+1; j<r[1]; ++j){
+        canvas[ j ] = '¯';
+        canvas[ ((lines-1)*MAXWIDTH) + j ] = '_';
+      }
+    }
+
+    return canvas;
+  }
+}
+
+function plotMedian(v){
+  return function(canvas){
+    var lines = Math.floor( canvas.length / MAXWIDTH );
+    for (var i=0;i<lines;++i){
+      canvas[ (i*MAXWIDTH) + v ] = '|';
+    }
+    return canvas;
+  }
+}
+
+function plotMean(v){
+  return function(canvas){
+    var lines = Math.floor( canvas.length / MAXWIDTH );
+    var midline = Math.floor( lines / 2 );
+    
+    canvas[ (midline*MAXWIDTH) + v ] = 'μ';
+
+    return canvas;
+  }
+}
+
+// note assumes sorted outliers
+function plotOutliers(vs){
+  return function(canvas){
+    var lines = Math.floor( canvas.length / MAXWIDTH );
+    var midline = Math.floor( lines / 2 );
+   
+    var last, n;
+    for (var i=0;i<vs.length;++i){
+      if (last == vs[i]){ n++; } else { n=1; }
+      canvas[ (midline*MAXWIDTH) + vs[i] ] = (n < 10 ? ''+n : '+');
+      last = vs[i];
+    }
+
+    return canvas;
+  }
+}
+
+
+// extract below this line
+
+function range(qs){
+  return [qs[0], qs[qs.length-1]];
+}
+
+function median(qs){
+  return qs[(qs.length-1)/2];
+}
+
+function iqr(qs){
+  var mid = (qs.length-1)/2;
+  return [qs[mid-1], qs[mid+1]];
+}
+
+function outliers(whiskers,vector){
+  var below = vector.filter( lt(whiskers[0]) )
+    , above = vector.filter( gt(whiskers[1]) );
+  return below.concat(above);
+}
+
+function tukeyWhiskers(qs,vector){
+  var iq = iqr(qs)
+    , lower = iq[0]
+    , upper = iq[1]
+    , iqdist = upper - lower
+    , dist = iqdist * 1.5;
+
+  var vs_lower = vector.filter( gte(lower-dist) );
+  var vs_upper = vector.filter( lte(upper+dist) );
+
+  /*
+  console.log('tukey lower %o', lower-dist);
+  console.log('tukey upper %o', upper+dist);
+  */
+
+  var w_lower = head( numericSort(vs_lower) );
+  var w_upper = last( numericSort(vs_upper) );
+
+  return [w_lower, w_upper];
+}
+
+function scale(d,r){
+  var factor = (r[1] - r[0]) / (d[1] - d[0]) ;
+  return function(n){
+    return Math.round( ((n - d[0]) * factor) + r[0] );
+  }
+}
+
+function zeroArray(v,n){
+  var ret = Array(n);
+  for (var i=0;i<n;++i){
+    ret[i] = v;
+  }
+  return ret;
+}
+
+function segment(n,list){
+  var ret = [];
+  if (n < 1) throw new TypeError('first parameter must be positive integer');
+  for (var i=0;i<Math.ceil(list.length/n);++i){
+    ret.push( list.slice(i*n,(i*n)+n) );
+  }
+  return ret;
+}
+
+function gte(x){
+  return function(y){
+    return y >= x;
+  }
+}
+
+function lte(x){
+  return function(y){
+    return y <= x;
+  }
+}
+
+function gt(x){
+  return function(y){
+    return y > x;
+  }
+}
+
+function lt(x){
+  return function(y){
+    return y < x;
+  }
+}
+
